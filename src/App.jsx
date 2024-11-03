8184bd1e3d39ea7c78a8c43131b5db4b60cc845f
ryolingo <ryomatsuya04@gmail.com>
2024-11-03 14:37:49 +0900

    Dog　imgを取得

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
 // DO NOT DELETE
 
 import './App.css'
-
+import React, { useState, useEffect } from 'react'
 /**
  * @type {() => JSX.Element}
  */
+
+const header = ' Dog アプリ'
+
 export const App = () => {
+  const [dogImage, setDogImage] = useState('')
+
+  useEffect(() => {
+    const fetchDoGImage = async () => {
+      const response = await fetch('https://dog.ceo/api/breeds/image/random')
+      const data = await response.json()
+      setDogImage(data.message)
+    }
+    fetchDoGImage()
+  }, [])
+
   return (
-    <div>
-      <h2>Hello, world!</h2>
-    </div>
+    <>
+      <header>
+        <div>
+          <h2>{header}</h2>
+        </div>
+      </header>
+      <div>
+        <p>犬の画像を表示するサイトです</p>
+        <img src={dogImage} alt="dog" />
+      </div>
+    </>
   )
 }
