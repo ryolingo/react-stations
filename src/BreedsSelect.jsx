d6daee8e6c07f0cfa2d894303046855a1293786c
ryolingo <ryomatsuya04@gmail.com>
2024-12-15 19:52:06 +0900

    Breed

--- src/BreedsSelect.jsx
+++ src/BreedsSelect.jsx
@@ -1,7 +1,14 @@
-// @ts-check
+import React from 'react'
 
-export const BreedsSelect = () => {
-  return <></>
+export const BreedsSelect = ({ breeds, selectedBreed, onBreedChange }) => {
+  return (
+    <select value={selectedBreed} onChange={e => onBreedChange(e.target.value)}>
+      <option value="">-- 犬種を選択してください --</option>
+      {breeds.map(breed => (
+        <option key={breed} value={breed}>
+          {breed}
+        </option>
+      ))}
+    </select>
+  )
 }
-
-export default BreedsSelect
