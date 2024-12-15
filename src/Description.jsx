347e6f05bdea687035f32ab5431633fb2169fc46
ryolingo <ryomatsuya04@gmail.com>
2024-12-15 19:52:14 +0900

    Description

--- src/Description.jsx
+++ src/Description.jsx
@@ -1,7 +1,28 @@
-// @ts-check
+import { DogImage } from './DogImage'
+import { useState } from 'react'
 
 export const Description = () => {
-  return <></>
-}
+  const [dogUrl, setDogUrl] = useState(
+    'https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg',
+  )
 
-export default Description
+  const fetchDogImage = () => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(response => response.json())
+      .then(data => {
+        setDogUrl(data.message)
+      })
+      .catch(error => {
+        console.error('Error:', error)
+      })
+  }
+  return (
+    <>
+      <p>犬の画像を表示するサイトです</p>
+      <DogImage url={dogUrl} />
+      <button onClick={fetchDogImage} className="button">
+        更新
+      </button>
+    </>
+  )
+}
