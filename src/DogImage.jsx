7f0f8bfaeeb5b26513da07a79ccc10506162794f
ryolingo <ryomatsuya04@gmail.com>
2024-12-15 19:52:19 +0900

    DogImage

--- src/DogImage.jsx
+++ src/DogImage.jsx
@@ -1,7 +1,9 @@
-// @ts-check
-
-export const DogImage = () => {
-  return <></>
+export const DogImage = ({ imageUrl }) => {
+  return (
+    <img
+      src={imageUrl || 'https://via.placeholder.com/300'} // デフォルト画像を設定
+      alt="A random dog"
+      style={{ width: '300px', height: 'auto' }}
+    />
+  )
 }
-
-export default DogImage
